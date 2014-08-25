fc3127cf5cb06671094e8a3a43b0a896b692bdeb
SKazmierczak <stephane.kazmierczak@gmail.com>
2014-08-26 00:52:41 +0100

    update from home

--- pyhton-r/test3-topGo.R
+++ pyhton-r/test3-topGo.R
@@ -1,5 +1,4 @@
 library(topGO)
-library(qvalue)
 
 setwd("/work/bio-informatics/pyhton-r")
 
@@ -7,44 +6,55 @@ file <- 'gene_association.goa_human_noHeader'
 sep <- "\t"
 IDsep <- ","
 
-a <- read.delim(file = file, header = FALSE, quote = "", 
-                sep = sep, colClasses = "character")
+a <- read.delim(file = file,
+                header = FALSE,
+                quote = "", 
+                sep = sep,
+                colClasses = "character")
 
 map <- a[, 5]
-names(map) <- a[, 2]
+names(map) <- a[, 3]
 map <- lapply(map, function(x) gsub(" ", "", strsplit(x, split = IDsep)[[1]]))
 
 
 my.data <- read.csv('gene_data_hs.csv')
 geneList <- my.data$pvalue
-names(geneList) <- my.data$DB.Object.ID
+names(geneList) <- my.data$DB.Object.Symbol
 
 topDiffGenes = function(allScore) {
   return (allScore < 0.01)
 }
 
-topDiffGenes(geneList)
-
-sampleGOdataBP <- new("topGOdata",
+GOdataBP <- new("topGOdata",
                     description = "Simple session",
-                    ontology = "BP", 
+                    ontology = "CC", 
                     geneSelectionFun = topDiffGenes,
                     allGenes = geneList, 
+                    nodeSize = 1,
                     annot = annFUN.gene2GO,
                     gene2GO = map
 )
-resultFisherBP <- runTest(sampleGOdataBP, algorithm = "classic", statistic = "fisher")
+resultFisherBP <- runTest(GOdataBP,
+                          algorithm = "classic",
+                          statistic = "fisher")
+
+
+
+allRes <- GenTable(GOdataBP,
+                   classic = resultFisherBP,
+                   orderBy = "classic",
+                   ranksOf = "classicFisher",
+                   topNodes = 10)
+
 
 """
+library(qvalue)
 s <- score(resultFisherBP)
 pval <- c()
 for (i in 1:length(s)){
-  pval[i] <- s[i][1]
+pval[i] <- s[i][1]
 }
 p.adjust(pval, method='fdr')
 """
-
-
-
 
 
---
+++ pyhton-r/test4-topGo.R
@@ -0,0 +1,9 @@
+library(topGO)
+library(ALL)
+data(ALL)
+
+BPterms <- ls(GOBPTerm)
+head(BPterms)
+
+
+?head
